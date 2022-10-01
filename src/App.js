8b1845a35d166c985a2c522a24cf65508c8e500e
Pedro Marques <ppmarq1@gmail.com>
2022-10-01 21:22:28 +0100

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Greeting from './components/Greeting';
+import Greeting from './components/greeting';
 
 export default function App() {
   return (
